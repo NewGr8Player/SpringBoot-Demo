a66a05835c7798147f4635481fcbc4624e86cd4d
NewGr8Player <273221594@qq.com>
2018-07-15 00:28:07 +0800

    Edit     - 重构JWT部分代码     - 修改pom文件     - 修改SpringBoot入口文件

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ TODO List
 - [x] 整合Druid连接池
 - [x] 整合MyBatisPlus
 - [x] 整合Shiro与JWT
-- [ ] 整合Reids 
+- [ ] 整合Redis 
--- src/main/java/com.xavier/api/LoginController.java
+++ src/main/java/com.xavier/api/LoginController.java
@@ -1,37 +1,37 @@
 package com.xavier.api;
 
 import com.xavier.common.ResultMap;
-import com.xavier.common.util.JWTUtil;
+import com.xavier.common.jwt.JWTGen;
 import com.xavier.dao.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/auth")
 public class LoginController {
 
-	@Autowired
-	private UserMapper userMapper;
-	@Autowired
-	private ResultMap resultMap;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private ResultMap resultMap;
+    @Autowired
+    private JWTGen jwtGen;
 
-	@PostMapping("/login")
-	public ResultMap login(@RequestParam("username") String username,
-	                       @RequestParam("password") String password) {
-		String realPassword = userMapper.getPassword(username);
-		if (realPassword == null) {
-			return resultMap.fail().code(401).message("用户名错误");
-		} else if (!realPassword.equals(password)) {
-			return resultMap.fail().code(401).message("密码错误");
-		} else {
-			return resultMap.success().code(200).message(JWTUtil.createToken(username));
-		}
-	}
+    @PostMapping("/login")
+    public ResultMap login(@RequestParam("username") String username,
+                           @RequestParam("password") String password) {
+        String realPassword = userMapper.getPassword(username);
+        if (realPassword == null) {
+            return resultMap.fail().code(401).message("用户名错误");
+        } else if (!realPassword.equals(password)) {
+            return resultMap.fail().code(401).message("密码错误");
+        } else {
+            return resultMap.success().code(200).message(jwtGen.createToken(username));
+        }
+    }
 
-	@GetMapping(path = "/unauthorized/{message}")
-	public ResultMap unauthorized(@PathVariable String message) {
-		return resultMap.success().code(401).message(message);
-	}
+    @GetMapping(path = "/unauthorized/{message}")
+    public ResultMap unauthorized(@PathVariable String message) {
+        return resultMap.success().code(401).message(message);
+    }
 }
