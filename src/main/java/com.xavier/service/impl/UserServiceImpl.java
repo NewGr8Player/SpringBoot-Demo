361ebd8efe0d6684e5b5a6bce9de1c1758fdd515
NewGr8Player <273221594@qq.com>
2018-08-14 15:06:19 +0800

    Add     - 添加一个使用样例

--- src/main/java/com.xavier/service/impl/UserServiceImpl.java
+++ src/main/java/com.xavier/service/impl/UserServiceImpl.java
@@ -7,24 +7,27 @@ import com.xavier.dao.UserMapper;
 import com.xavier.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
 /**
  * User Service 实现
+ * <br />
+ * 事务注解中事务管理器名称{@link com.xavier.config.DruidConfig#regTransactionManager}上的注解
  *
  * @author NewGr8Player
  */
 @Service
-@Transactional(readOnly = true)
+@Transactional(readOnly = true, transactionManager = "xatx", propagation = Propagation.REQUIRED, rollbackFor = {java.lang.Exception.class})
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserMapper userMapper;
+	@Autowired
+	private UserMapper userMapper;
 
-    @DataSourceSwitch(DBTypeEnum.db2)
-    @Override
-    public User searchById(String id) {
-        return this.userMapper.selectById(id);
-    }
+	@DataSourceSwitch(DBTypeEnum.db2)
+	@Override
+	public User searchById(String id) {
+		return this.userMapper.selectById(id);
+	}
 }
